f4e95840f29b7ce09045c00518920cb5011ee3fe
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-01-21 13:34:04 +0300

    refactor: change return positions

--- src/index.js
+++ src/index.js
@@ -2,14 +2,13 @@ module.exports = function check(str, bracketsConfig) {
     const bracketsPatterns = bracketsConfig.map((config) => config.join(""));
 
     function bracketRecursion() {
+        if (str.length == 0) return true;
         for (const pattern of bracketsPatterns)
             if (str.includes(pattern))
-                bracketRecursion(
+                return bracketRecursion(
                     (str = str.replace(pattern, "")),
                     bracketsPatterns
                 );
-
-        if (str.length == 0) return true;
 
         return false;
     }
